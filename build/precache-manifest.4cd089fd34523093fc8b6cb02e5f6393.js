49a58e1111e6cba660048edaa66a3e9fe6e46251
Gonzalo Arrivi <gonzalo.arrivi@gmail.com>
2020-01-16 16:46:48 +0100

    Included built artifacts for azure

---
+++ build/precache-manifest.4cd089fd34523093fc8b6cb02e5f6393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e851ef3327cfb5e9085cb066ed3bc5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54e99b2889ec8f7b75c",
+    "url": "/static/js/2.c0deb58e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c0deb58e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca64dd66ec47c9633ace",
+    "url": "/static/js/main.7e047d54.chunk.js"
+  },
+  {
+    "revision": "bf263d10e529c2c836bb",
+    "url": "/static/js/runtime-main.d26f3f55.js"
+  }
+]);
