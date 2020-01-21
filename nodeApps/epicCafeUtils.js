b1d9a0c352f1d0b60bf92054deac34b3d0296411
Gonzalo Arrivi <gonzalo.arrivi@gmail.com>
2020-01-21 11:05:30 +0100

    Enhanced fetching and processing new data

--- nodeApps/epicCafeUtils.js
+++ nodeApps/epicCafeUtils.js
@@ -6,40 +6,38 @@ const readline = require("readline");
 
 function fetchEpicCafeCatalog(outputFilePath) {
   return new Promise((resolve, reject) => {
-    const browser = new Browser();
-    browser.visit("https://www.epicmadrid.com/nuestrosjuegos", () => {
-      var dataSource = browser
-        .queryAll("iframe")
-        .map(x => x.getAttribute("data-src"))
-        .find(x => x.indexOf("wix-visual-data.appspot.com") >= 0);
+    if (!fs.existsSync(outputFilePath)) {
+      const browser = new Browser();
+      browser.visit("https://www.epicmadrid.com/nuestrosjuegos", () => {
+        var dataSource = browser
+          .queryAll("iframe")
+          .map(x => x.getAttribute("data-src"))
+          .find(x => x.indexOf("wix-visual-data.appspot.com") >= 0);
 
-      const [, queryString] = dataSource.split("?");
-      const queryStringEntries = queryString
-        .split("&")
-        .map(x => x.split("="))
-        .filter(([name]) => name === "compId" || name === "instance");
-      const url =
-        "https://wix-visual-data.appspot.com/app/file" +
-        "?" +
-        queryStringEntries.map(entry => entry.join("=")).join("&");
-      https.get(url, res => {
-        res.setEncoding("utf-8");
-        res.pipe(fs.createWriteStream(outputFilePath));
-        // var fileResponse = "";
-        // res
-        //   .on("error", error => reject(error))
-        //   .on("data", data => {
-        //     fileResponse += data;
-        //   })
-        //   .on("end", () => {
-        //     resolve(fileResponse);
-        //   });
+        const [, queryString] = dataSource.split("?");
+        const queryStringEntries = queryString
+          .split("&")
+          .map(x => x.split("="))
+          .filter(([name]) => name === "compId" || name === "instance");
+        const url =
+          "https://wix-visual-data.appspot.com/app/file" +
+          "?" +
+          queryStringEntries.map(entry => entry.join("=")).join("&");
+        https.get(url, res => {
+          res.setEncoding("utf-8");
+          res
+            .pipe(fs.createWriteStream(outputFilePath))
+            .on("close", () => resolve(true));
+          res.on("error", error => reject(error));
+        });
       });
-    });
+    } else {
+      resolve(false);
+    }
   });
 }
 
-function readCatalogFile(inputFilePath, outputDirectoryPath) {
+function parseEpicCafeCatalog(inputFilePath, outputDirectoryPath) {
   const gameEntryKeys = ["name", "coordinates", "quantity", "players", "age"];
   const catalog = [];
   const keyValuesSets = gameEntryKeys.reduce(
@@ -97,9 +95,7 @@ function readCatalogFile(inputFilePath, outputDirectoryPath) {
     };
   };
   readline
-    .createInterface(
-      fs.createReadStream(inputFilePath)
-    )
+    .createInterface(fs.createReadStream(inputFilePath))
     .on("line", line => readLine(line))
     .on("close", () => {
       fs.writeFile(
@@ -119,5 +115,5 @@ function readCatalogFile(inputFilePath, outputDirectoryPath) {
 
 module.exports = {
   fetchEpicCafeCatalog,
-  readCatalogFile
+  parseEpicCafeCatalog
 };
--- nodeApps/main.js
+++ nodeApps/main.js
@@ -1,13 +1,34 @@
-var epicCafeUtils = require("./epicCafeUtils");
+const epicCafeUtils = require("./epicCafeUtils");
 const path = require("path");
 
-const inputDirectoryPath = path.resolve(__dirname, "input");
-const outputDirectoryPath = path.resolve(__dirname, "output");
+function fetchAndProcess() {
+  const inputDirectoryPath = path.resolve(__dirname, "input");
+  const outputDirectoryPath = path.resolve(__dirname, "output");
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const newFileName = `epicgames_${year}${[month, day]
+    .map(x => `${x < 10 ? "0" : ""}${x}`)
+    .join("")}.txt`;
+  var epicGamesFilePath = path.resolve(inputDirectoryPath, newFileName);
+  // epicCafeUtils.fetchEpicCafeCatalog(epicGamesFile);
+  epicCafeUtils.fetchEpicCafeCatalog(epicGamesFilePath).then(created => {
+    console.log(
+      created
+        ? `New file "${newFileName}" created with the fetched data`
+        : `File "${newFileName}" already exists, so the process is done using it`
+    );
+    epicCafeUtils.parseEpicCafeCatalog(epicGamesFilePath, outputDirectoryPath);
+  });
+}
 
-var epicGamesFile = path.resolve(
-  inputDirectoryPath,
-  // `epicgames_${new Date().getTime()}.txt`
-  `epicgames_1579552743600.txt`
-);
-// epicCafeUtils.fetchEpicCafeCatalog(epicGamesFile);
-epicCafeUtils.readCatalogFile(epicGamesFile, outputDirectoryPath)
+const [, , command] = process.argv;
+switch (command) {
+  case "process": {
+    fetchAndProcess();
+    break;
+  }
+  default:
+    console.log(`Unrecognized "${command}" command`);
+}
