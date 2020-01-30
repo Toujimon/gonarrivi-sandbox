d71b8d3269f28eeda6e37854235683fee7b46b7e
Gonzalo Arrivi <gonzalo.arrivi@gmail.com>
2020-01-30 16:16:54 +0100

    Enhanced epic cafe manager
    The complete matching functionality
    has been enabled. Is ugly as hell,
    but functional.
    Have tested with some games and
    it is very cool :-)

--- nodeApps/app.js
+++ nodeApps/app.js
@@ -8,21 +8,66 @@ const {
 
 const port = 4000;
 const app = express();
+app.use(express.json());
 
 app.get("/api/catalog", (req, res) => {
-  Promise.all([
-    fileUtils.readJsonFromFile(processedCatalogFilePath),
-    fileUtils.readJsonFromFile(indexedBggCatalogPath),
-    fileUtils.readJsonFromFile(epicBggJoinPath)
-  ]).then(([epicCatalog, bggCatalog, epicBggJoin]) => {
+  getAllData().then(([epicCatalog, bggCatalog, epicBggJoin]) => {
     res.send({ epicCatalog, bggCatalog, epicBggJoin });
   });
 });
-// app.get("/*", (req, res) => {
-//   console.log("request received", req.url);
-//   res.send(`OK: ${req.path}`);
-// });
+
+app.put("/api/catalog", (req, res) => {
+  console.log(`PUT catalog entry match`, req.body);
+  const { id, bggMatchId } = req.body;
+  if (id && bggMatchId) {
+    getAllData().then(([, bggCatalog, epicBggJoin]) => {
+      if (epicBggJoin[id] && bggCatalog[bggMatchId]) {
+        epicBggJoin[id].bggMatchId = bggMatchId;
+        fileUtils.writeJsonToFile(epicBggJoin, epicBggJoinPath).then(() => {
+          res.sendStatus(200);
+        });
+      } else {
+        res.sendStatus(404);
+      }
+    });
+  } else {
+    res.sendStatus(500);
+  }
+});
+
+app.delete("/api/catalog", (req, res) => {
+  console.log(`DELETE catalog entry matches`, req.body);
+  const { id } = req.body;
+  if (id) {
+    getAllData().then(([, , epicBggJoin]) => {
+      const epicBggJoinEntry = epicBggJoin[id];
+      if (epicBggJoinEntry) {
+        if (epicBggJoinEntry.bggMatchId) {
+          epicBggJoin[id].foundBggMatches = [];
+          fileUtils.writeJsonToFile(epicBggJoin, epicBggJoinPath).then(() => {
+            res.sendStatus(200);
+          });
+        } else {
+          res.sendStatus(500);
+        }
+      } else {
+        res.sendStatus(404);
+      }
+    });
+  } else {
+    res.sendStatus(500);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
 });
+
+/*-- UTILITY FUNCTIONS --*/
+function getAllData() {
+  return Promise.all([
+    fileUtils.readJsonFromFile(processedCatalogFilePath),
+    fileUtils.readJsonFromFile(indexedBggCatalogPath),
+    fileUtils.readJsonFromFile(epicBggJoinPath)
+  ]);
+}
--- src/epic-cafe-manager.js
+++ src/epic-cafe-manager.js
@@ -2,17 +2,22 @@ import React from "react";
 import { StyledBox } from "./components/design-system";
 import {
   Typography,
-  List,
-  ListItem,
-  ListItemText,
   TableContainer,
   Table,
   TableBody,
   TableRow,
   TableCell,
-  ListItemSecondaryAction,
-  Button
+  Grid,
+  Toolbar,
+  Select,
+  Button,
+  IconButton,
+  TablePagination,
+  Checkbox,
+  FormControlLabel
 } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faSadTear, faSmile } from "@fortawesome/free-solid-svg-icons";
 
 export default function EpicCafeManager() {
   return (
@@ -22,8 +27,13 @@ export default function EpicCafeManager() {
         Management tools for the Epic Cafe game's catalog
       </Typography>
       <StyledBox>
-        <Typography variant="h3">Catalog</Typography>
-        <GamesCatalogContainer />
+        <Typography variant="h3">Board Game Geek matcher tool</Typography>
+        <Typography variant="subtitle2">
+          A tool to properly match the diferent entries of the Epic Cafe catalog
+          to the well known Board Game Geek database entries, so extra meta-data
+          about the different games can be retrieved.
+        </Typography>
+        <EpicBggMatcherContainer />
       </StyledBox>
     </React.Fragment>
   );
@@ -31,90 +41,285 @@ export default function EpicCafeManager() {
 
 /*-- PRIVATE COMPONENTS --*/
 /* To be refactored out if it seems like it may be useful on other modules */
-function GamesCatalog({ games }) {
+
+function EpicBggMatcherContainer() {
+  const [[catalog, mustFetch], setState] = React.useState([null, true]);
+  function setCatalog(catalog) {
+    setState([catalog, false]);
+  }
+  function refetchCatalog() {
+    setState([catalog, true]);
+  }
+  React.useEffect(() => {
+    if (mustFetch) {
+      fetch("/api/catalog")
+        .then(response => response.json().then(jsonData => jsonData))
+        .catch(error => {
+          console.log(error);
+          return error;
+        })
+        .then(({ epicCatalog, bggCatalog, epicBggJoin }) => {
+          const catalog = epicCatalog.map(
+            ({ id, name, players, age, coordinates }) => {
+              return {
+                id,
+                name,
+                players:
+                  players.type === "range" || players.type === "min"
+                    ? `${players.min} - ${players.max || ""}`
+                    : players.type,
+                age,
+                coordinates,
+                bggMatchId: epicBggJoin[id].bggMatchId,
+                foundBggMatches: epicBggJoin[id].foundBggMatches.map(
+                  matchId => bggCatalog[matchId]
+                )
+              };
+            }
+          );
+          setCatalog(catalog);
+        });
+    }
+  }, [mustFetch]);
+
+  function handleEpicBggMatchConfirm(id, bggMatchId) {
+    fetch("/api/catalog", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ id, bggMatchId })
+    }).then(res => {
+      if (res.status === 200) {
+        refetchCatalog();
+      } else {
+        console.error("Error while trying to update data:", res.statusText);
+      }
+    });
+  }
+
+  function handleEpicBggMatchesClean(id) {
+    fetch("/api/catalog", {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ id })
+    }).then(res => {
+      if (res.status === 200) {
+        refetchCatalog();
+      } else {
+        console.error("Error while trying to clean matches:", res.statusText);
+      }
+    });
+  }
+
+  return catalog === null ? (
+    "Loading catalog information, may take a while..."
+  ) : (
+    <EpicBggMatcher
+      games={catalog}
+      onEpicBggMatchConfirm={handleEpicBggMatchConfirm}
+      onEpicBggMatchesClean={handleEpicBggMatchesClean}
+    />
+  );
+}
+
+const PAGE_SIZE = 25;
+const FILTERS = [
+  ["All", null],
+  ["No matches", x => !x.foundBggMatches.length],
+  ["Just one match", x => x.foundBggMatches.length === 1],
+  ["Multiple matches", x => x.foundBggMatches.length > 1]
+];
+
+function EpicBggMatcher({
+  games,
+  onEpicBggMatchConfirm,
+  onEpicBggMatchesClean
+}) {
+  const [
+    [page, appliedFilterIndex, hideConfirmedMatches],
+    setState
+  ] = React.useState([0, 0, true]);
+  const setPage = page =>
+    setState([page, appliedFilterIndex, hideConfirmedMatches]);
+  const setAppliedFilterIndex = appliedFilterIndex =>
+    setState([0, appliedFilterIndex, hideConfirmedMatches]);
+
+  const [, appliedFilter = null] = FILTERS[appliedFilterIndex] || [];
+
+  const [displayedGames, filteredGamesCount] = React.useMemo(() => {
+    const filteredGames = (!appliedFilter
+      ? games
+      : games.filter(appliedFilter)
+    ).filter(
+      !hideConfirmedMatches
+        ? x => x
+        : x => !x.bggMatchId || x.foundBggMatches.length
+    );
+    const displayedGames = filteredGames.slice(
+      page * PAGE_SIZE,
+      (page + 1) * PAGE_SIZE
+    );
+    return [displayedGames, filteredGames.length];
+  }, [games, page, appliedFilter, hideConfirmedMatches]);
+
+  React.useEffect(() => {
+    if (page > 0 && !displayedGames.length) {
+      setPage(0);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [displayedGames, page]);
+
   return (
     <TableContainer component={StyledBox}>
+      <Grid container>
+        <Grid item md={8}>
+          <Toolbar>
+            <Select
+              value={appliedFilterIndex}
+              native
+              onChange={event =>
+                setAppliedFilterIndex(Number(event.currentTarget.value))
+              }
+            >
+              {FILTERS.map(([text], index) => (
+                <option key={index} value={index}>
+                  {text}
+                </option>
+              ))}
+            </Select>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={hideConfirmedMatches}
+                  onChange={event =>
+                    setState(([, , prevValue]) => [
+                      page,
+                      appliedFilterIndex,
+                      !prevValue
+                    ])
+                  }
+                />
+              }
+              label="Hide confirmed matches"
+            />
+          </Toolbar>
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <TablePagination
+            component="div"
+            count={filteredGamesCount}
+            rowsPerPage={PAGE_SIZE}
+            rowsPerPageOptions={[PAGE_SIZE]}
+            page={page}
+            onChangePage={(event, newPage) => setPage(newPage)}
+          />
+        </Grid>
+      </Grid>
+
       <Table>
         <TableBody>
-          {games.map(game => (
-            <TableRow key={game.id}>
-              <TableCell vertical-align="top">{game.name}</TableCell>
-              <TableCell vertical-align="top">
-                {game.bggMatchId || "None"}
-              </TableCell>
-              <TableCell>
-                {!!game.foundBggMatches.length && (
-                  <List>
-                    {game.foundBggMatches.map(match => {
-                      const { id, name, yearpublished } = match;
-                      return (
-                        <ListItem key={id}>
-                          <ListItemText
-                            primary={
-                              typeof name[0] === "string"
-                                ? name[0]
-                                : name[0]["_"]
-                            }
-                            secondary={
-                              yearpublished
-                                ? yearpublished[0]
-                                : "Unknown release year"
-                            }
-                          />
-                          <ListItemSecondaryAction>
-                            <Button
-                              onClick={() =>
-                                window.open(
-                                  `https://boardgamegeek.com/boardgame/${id}`
-                                )
-                              }
-                            >
-                              See BGG Entry
-                            </Button>
-                          </ListItemSecondaryAction>
-                        </ListItem>
-                      );
-                    })}
-                  </List>
-                )}
-              </TableCell>
-            </TableRow>
-          ))}
+          {displayedGames.map(
+            ({ id, name, players, age, bggMatchId, foundBggMatches }) => (
+              <TableRow key={id}>
+                <TableCell align="left">
+                  <Typography>{name}</Typography>
+                  <Typography variant="caption">
+                    Players: {players}, Age: {age}
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  {bggMatchId ? (
+                    <IconButton
+                      onClick={() =>
+                        window.open(
+                          `https://boardgamegeek.com/boardgame/${bggMatchId}`
+                        )
+                      }
+                    >
+                      <FontAwesomeIcon icon={faEye} />
+                    </IconButton>
+                  ) : (
+                    <Typography>None</Typography>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {/* {JSON.stringify(game.foundBggMatches)} */}
+                  {foundBggMatches.length ? (
+                    <BggMatchSelector
+                      matches={foundBggMatches}
+                      currentMatch={bggMatchId}
+                      onMatchConfirm={matchId =>
+                        onEpicBggMatchConfirm(id, matchId)
+                      }
+                      onMatchesClean={() => onEpicBggMatchesClean(id)}
+                    />
+                  ) : (
+                    <React.Fragment>
+                      <Typography component="span">
+                        {!bggMatchId
+                          ? "No matches found"
+                          : "Match already confirmed"}{" "}
+                        <FontAwesomeIcon
+                          icon={!bggMatchId ? faSadTear : faSmile}
+                        />
+                      </Typography>
+                    </React.Fragment>
+                  )}
+                </TableCell>
+              </TableRow>
+            )
+          )}
         </TableBody>
       </Table>
     </TableContainer>
   );
 }
 
-function GamesCatalogContainer() {
-  const [catalog, setCatalog] = React.useState(null);
-  React.useEffect(() => {
-    fetch("/api/catalog")
-      .then(response => response.json().then(jsonData => jsonData))
-      .catch(error => {
-        console.log(error);
-        return error;
-      })
-      .then(({ epicCatalog, bggCatalog, epicBggJoin }) => {
-        setCatalog(
-          epicCatalog.map(({ id, name }) => {
-            return {
-              id,
-              name,
-              bggMatchId: epicBggJoin[id].bggMatchId,
-              foundBggMatches: epicBggJoin[id].foundBggMatches.map(
-                matchId => bggCatalog[matchId]
-              )
-            };
-          })
-        );
-      });
-  }, []);
-
-  return catalog === null ? (
-    "Loading catalog information, may take a while..."
-  ) : (
-    <GamesCatalog games={catalog} />
+function BggMatchSelector({
+  matches,
+  currentMatch = null,
+  onMatchConfirm,
+  onMatchesClean
+}) {
+  const [selected, setSelected] = React.useState(currentMatch);
+  // const currentlySelected = matches.find(x => x.id === selected);
+  return (
+    <Toolbar>
+      <Select
+        value={selected || ""}
+        native
+        onChange={event => setSelected(event.currentTarget.value || null)}
+      >
+        <option value=""></option>
+        {matches.map(({ id, name, yearpublished }) => (
+          <option key={id} value={id}>
+            {typeof name[0] === "string" ? name[0] : name[0]["_"]} (
+            {yearpublished ? yearpublished[0] : "???"})
+          </option>
+        ))}
+      </Select>
+      {!!selected && (
+        <React.Fragment>
+          <IconButton
+            onClick={() =>
+              window.open(`https://boardgamegeek.com/boardgame/${selected}`)
+            }
+          >
+            <FontAwesomeIcon icon={faEye} />
+          </IconButton>
+          {selected !== currentMatch ? (
+            <Button onClick={() => onMatchConfirm(selected)}>
+              Confirm match
+            </Button>
+          ) : (
+            <Button onClick={() => onMatchesClean()}>Clean matches</Button>
+          )}
+        </React.Fragment>
+      )}
+    </Toolbar>
   );
 }
 
