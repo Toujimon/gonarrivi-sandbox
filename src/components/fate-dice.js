8046d0122af1b5dddc8b595a3f1fcf31b8e24a2f
Gonzalo Arrivi Duarte <gonarrivi@MacBook-Pro-de-Gonzalo.local>
2020-02-10 15:01:10 +0100

    Included structural testing wit storyshots
    Also, removed the sample stories
    introduced by storybook.

--- src/components/fate-dice.js
+++ src/components/fate-dice.js
@@ -6,7 +6,7 @@ import { theme } from "./design-system";
 import FateDie from "./fate-die";
 export { DEFAULT_SIZE } from "./fate-die";
 
-const CLICK_ANIMATION_MILLISECONDS = 100;
+// const CLICK_ANIMATION_MILLISECONDS = 100;
 
 const StyledWrapper = styled.span`
   display: inline-flex;
@@ -42,85 +42,48 @@ StyledSelectableFateDie.defaultProps = {
   theme
 };
 
-const clickedKeyFrames = keyframes`
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-10px);
-  }
-  100%{
-    transform: translateY(0);
-  }
-`;
+// const clickedKeyFrames = keyframes`
+//   0% {
+//     transform: translateY(0);
+//   }
+//   50% {
+//     transform: translateY(-10px);
+//   }
+//   100%{
+//     transform: translateY(0);
+//   }
+// `;
 
-const StyledSelectionChangedFateDice = styled(StyledSelectableFateDie)`
-  animation: ${css`
-    ${clickedKeyFrames} ${CLICK_ANIMATION_MILLISECONDS}ms linear
-  `};
-`;
+// const StyledSelectionChangedFateDice = styled(StyledSelectableFateDie)`
+//   animation: ${css`
+//     ${clickedKeyFrames} ${CLICK_ANIMATION_MILLISECONDS}ms linear
+//   `};
+// `;
 
-class FateDice extends React.Component {
-  state = {
-    selectionChangedIndex: -1
-  };
-  _memoizedHandlers = {};
+function FateDice({
+  dice, onDieSelect
+}) {
 
-  _onSelectionChange = (handler, index, ...args) => {
-    if (this.state.selectionChangedIndex !== index) {
-      this.setState({ selectionChangedIndex: index }, () =>
-        setTimeout(
-          () =>
-            this.setState({ selectionChangedIndex: -1 }, () =>
-              handler(index, ...args)
-            ),
-          CLICK_ANIMATION_MILLISECONDS
-        )
-      );
-    }
-  };
+  return (
+    <StyledWrapper>
+      <span>
+        {dice.map((x, i) =>
+          <StyledSelectableFateDie
+            key={i}
+            value={x.value}
+            selected={x.selected}
+            onClick={(...args) => onDieSelect(i, ...args)}
+          />)}
+      </span>
+    </StyledWrapper>
+  );
 
-  _getMemoizedHandler = (index, handler) => {
-    if (!this._memoizedHandlers[index]) {
-      this._memoizedHandlers[index] = handler
-        ? (...args) => this._onSelectionChange(handler, index, ...args)
-        : null;
-    }
-    return this._memoizedHandlers[index];
-  };
-
-  render() {
-    const { dice, onDieSelect } = this.props;
-    const { selectionChangedIndex } = this.state;
-    return (
-      <StyledWrapper>
-        <span>
-          {dice.map((x, i) =>
-            selectionChangedIndex !== i ? (
-              <StyledSelectableFateDie
-                value={x.value}
-                selected={x.selected}
-                onClick={this._getMemoizedHandler(i, onDieSelect)}
-              />
-            ) : (
-              <StyledSelectionChangedFateDice
-                value={x.value}
-                selected={x.selected}
-              />
-            )
-          )}
-        </span>
-      </StyledWrapper>
-    );
-  }
 }
 
 FateDice.propTypes = {
-  rollId: PropTypes.any.isRequired,
   dice: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.number.isRequired,
-      rerollCount: PropTypes.number.isRequired,
       selected: PropTypes.bool
     })
   ).isRequired,
